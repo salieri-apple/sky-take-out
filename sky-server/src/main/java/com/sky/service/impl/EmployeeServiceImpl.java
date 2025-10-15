e54700350b687b54c2bedc822a79936225d3c6b4
tjp <1548641277@qq.com>
2025-10-15 15:47:16 +0800

    编辑员工业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -132,5 +132,25 @@ public class EmployeeServiceImpl implements EmployeeService {
         employeeMapper.update(employee);
     }
 
+    //根据id查询员工信息
+    @Override
+    public Employee getById(Long id) {
+        Employee employee = employeeMapper.geyById(id);
+        employee.setPassword("****");
+        return employee;
+    }
+
+    //编辑员工信息
+    @Override
+    public void update(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeDTO, employee);
+
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(BaseContext.getCurrentId());
+
+        employeeMapper.update(employee);
+    }
+
 
 }
